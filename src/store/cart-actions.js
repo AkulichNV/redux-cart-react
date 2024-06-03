70686146ce6b9119debacf0689cf53d7c07bfc9b
Natalia <sh.nat.vlad@gmail.com>
2024-06-03 10:15:13 +0500

    fix: fix fetching data useless sending requests

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,11 @@ export const fetchCartData = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity,
+
+            }));
         } catch (error) {
             dispatch(uiActions.showNotification({
                 status: 'error',
@@ -42,7 +46,10 @@ export const sendCartData = (cart) => {
         const sendRequest = async () => {
             const response = await fetch('https://redux-cart-react-default-rtdb.firebaseio.com/cart.json', {
                 method: 'PUT',
-                body: JSON.stringify(cart),
+                body: JSON.stringify({
+                    items: cart.items, 
+                    totalQuantity: cart.totalQuantity
+                }),
             });
     
             if(!response.ok) {
